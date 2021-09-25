bbdcb4308862d066c8c38362d0f95747a9985b13
Gabriel Rezende <gabrielrezende736@gmail.com>
2021-09-25 10:57:25 -0300

    inicial commit

---
+++ app/src/main/java/com/example/supportstudysong/activity/Correction_activity.kt
@@ -0,0 +1,12 @@
+package com.example.supportstudysong.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.supportstudysong.R
+
+class Correction_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_correction_activity)
+    }
+}
